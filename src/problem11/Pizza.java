8834d0bc8f60940c0ae6e1bb542a8db4ebd406ae
BlastoiseBlue <emmet@stanevich.com>
2017-10-24 13:50:34 -0400

    Built Pizza class

--- src/problem11/Pizza.java
+++ src/problem11/Pizza.java
@@ -1,5 +1,86 @@
 package problem11;
 
 public class Pizza {
-	
+	private int size = 0;
+	private int numCheese = 0;
+	private int numPepperoni = 0;
+	private int numHam = 0;
+	private double baseCost = 10.0; // Base cost of the pizza
+	private double sizeCost = 2.0; // Cost of increasing size by 1 level
+	private double toppingCost = 2.0; // Cost per topping
+
+	public String pizzaSize() {
+		switch (size) {
+		case 0:
+			return "Small";
+		case 1:
+			return "Medium";
+		case 2:
+			return "Large";
+		default:
+			return null;
+		}
+	}
+
+	public void setSize(int inputSize) {
+		if (inputSize >= 0 && inputSize <= 2)
+			size = inputSize;
+		else
+			size = 0;
+	}
+
+	public void setSize(String inputSize) {
+		switch (inputSize.toLowerCase()) {
+		case "small":
+			size = 0;
+			break;
+		case "medium":
+			size = 1;
+			break;
+		case "large":
+			size = 2;
+			break;
+		default:
+			size = 0;
+		}
+	}
+
+	public void setCheese(int inputCheese) {
+		numCheese = inputCheese;
+	}
+
+	public void setPepperoni(int inputPepperoni) {
+		numPepperoni = inputPepperoni;
+	}
+
+	public void setHam(int inputHam) {
+		numHam = inputHam;
+	}
+
+	public double calcCost() {
+		return (baseCost + (size * sizeCost) + ((numCheese + numPepperoni + numHam) * toppingCost));
+	}
+
+	public void getDescription() {
+		System.out.println(pizzaSize() + " pizza: " + (baseCost + (size * sizeCost)));
+		System.out.println(numCheese + " cheese toppings: " + numCheese * toppingCost);
+		System.out.println(numPepperoni + " pepperoni toppings: " + numPepperoni * toppingCost);
+		System.out.println(numHam + " ham toppings: " + numHam * toppingCost);
+		System.out.println("Total cost: " + calcCost());
+	}
+
+	Pizza() {
+
+	}
+
+	Pizza(int inputSize) {
+		setSize(inputSize);
+	}
+
+	Pizza(int inputSize, int inputCheese, int inputPepperoni, int inputHam) {
+		setSize(inputSize);
+		setCheese(inputCheese);
+		setPepperoni(inputPepperoni);
+		setHam(inputHam);
+	}
 }
